acbedef69f8233bec215632e0204442ae3a81c8d
Pooja Shelke <poojashelke43@gmail.com>
2023-07-21 18:25:16 +0530

    updated code

--- app/src/main/java/com/example/demoapp/Constants.kt
+++ app/src/main/java/com/example/demoapp/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     //TODO base URL
     const val  BASE_URL="https://api.openweathermap.org/data/2.5/"
     //TODO open weather api
-    const val  API_KEY="4598cc4a82a09a178085287d07c0500c"
+    const val  API_KEY="{WEATHER API KEY}"
     //TODO registered on https://opencagedata.com/ and get the key for places api
-    const val  PLACES_API_KEY="{GET YOUR PLACES API KEY FROM ABOVE WEBSITE FOR FREE}"
+    const val  PLACES_API_KEY="{PLACES API KEY}"
 }
